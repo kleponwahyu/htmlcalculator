af4c679a5db28d676cffbb8c6be7498bba0b592e
Wahyu Berlianto <wahyoeberliantoe@gmail.com>
2018-08-31 14:18:48 +0700

    Adding some comments in the code, but it's not complete yet...

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-var x, y, check = false, operasi;
+var x, y, check = false, operasi;		//Declaring variable
 
-function koma(){
+function koma(){		//Declaring a coma function, will add this later for sure...
 	
 }
-function satu(){
+function satu(){		//Declaring a function for '1' button. Satu means one
 	document.getElementById('hasil').innerHTML = "1";
-	if(check){
+	if(check){		//Using if else and check variable because we need to know whether we fill the x variable or the y variable
 		y = 1.0;
 	} else{
 		x = 1.0;
 	}
 }
-function dua(){
+function dua(){		//Declaring a function for '2' button. Dua means two
 	document.getElementById('hasil').innerHTML = "2";
 	if(check){
 		y = 2.0;
@@ -19,7 +19,7 @@ function dua(){
 		x = 2.0;
 	}
 }
-function tiga(){
+function tiga(){		//Declaring a function for '3' button. Tiga means three
 	document.getElementById('hasil').innerHTML = "3";
 	if(check){
 		y = 3.0;
@@ -27,7 +27,7 @@ function tiga(){
 		x = 3.0;
 	}
 }
-function empat(){
+function empat(){		//Declaring a function for '4' button. Empat means four
 	document.getElementById('hasil').innerHTML = "4";
 	if(check){
 		y = 4.0;
@@ -35,7 +35,7 @@ function empat(){
 		x = 4.0;
 	}
 }
-function lima(){
+function lima(){		//Declaring a function for '5' button. Lima menas five
 	document.getElementById('hasil').innerHTML = "5";
 	if(check){
 		y = 5.0;
@@ -43,7 +43,7 @@ function lima(){
 		x = 5.0;
 	}
 }
-function enam(){
+function enam(){		//Declaring a function for '6' button. Enam means six
 	document.getElementById('hasil').innerHTML = "6";
 	if(check){
 		y = 6.0;
@@ -51,7 +51,7 @@ function enam(){
 		x = 6.0;
 	}
 }
-function tujuh(){
+function tujuh(){		//Declaring a function for '7' button. Tujuh means seven
 	document.getElementById('hasil').innerHTML = "7";
 	if(check){
 		y = 7.0;
@@ -59,7 +59,7 @@ function tujuh(){
 		x = 7.0;
 	}
 }
-function delapan(){
+function delapan(){		//Declaring a function for '8' button. Delapan means eight
 	document.getElementById('hasil').innerHTML = "8";
 	if(check){
 		y = 8.0;
@@ -67,7 +67,7 @@ function delapan(){
 		x = 8.0;
 	}
 }
-function sembilan(){
+function sembilan(){		//Declaring a function for '9' button. sembilan means nine
 	document.getElementById('hasil').innerHTML = "9";
 	if(check){
 		y = 9.0;
@@ -75,7 +75,7 @@ function sembilan(){
 		x = 9.0;
 	}
 }
-function nol(){
+function nol(){		//Declaring a function for '0' button. Nol means zero
 	document.getElementById('hasil').innerHTML = "0";
 	if(check){
 		y = 0.0;
@@ -84,31 +84,31 @@ function nol(){
 	}
 }
 
-function tambah(){
+function tambah(){		//Declaring a function for addition button
 	check = true;
 	document.getElementById('hasil').innerHTML = "+";
 	operasi = 0;
 }
-function kurang(){
+function kurang(){		//Declaring a function for subtraction button
 	check = true;
 	document.getElementById('hasil').innerHTML = "-";
 	operasi = 1;
 }
-function kali(){
+function kali(){		//Declaring a function for multiplication button
 	check = true;
 	document.getElementById('hasil').innerHTML = "x";
 	operasi = 2;
 }
-function bagi(){
+function bagi(){		//Declaring a function for division button
 	check = true;
 	document.getElementById('hasil').innerHTML = ":";
 	operasi = 3;
 }
-function reset(){
+function reset(){		//Declaring a function for resetting x and y variable
 	document.getElementById('hasil').innerHTML = "0";
 	x = y = 0.0;
 }
-function hasil(){
+function hasil(){		//Declaring a function for the final operation
 	switch(operasi){
 		case 0:
 		operasi = x + y;
